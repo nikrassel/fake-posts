089659372d43a58e92be5c476fab39615377fc60
nikrassel <nikrassel@mail.ru>
2023-05-28 11:03:03 +0300

    add additional info cards

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,21 +1,10 @@
 import React from "react"
 import UserCard from "../userinfo/userPage/userCard"
 import UpperNavBar from "../ui/upperNavBar"
+import { currentUser, additionalInfo } from "./myInfo"
+import InfoCard from "./infoCard"
 
 const About = () => {
-    const currentUser = {
-        username: "Igor_Aparin",
-        name: "Игорь Апарин",
-        email: "igor.aparin@bk.ru",
-        address: {
-            zipcode: 602265,
-            city: "Муром",
-            street: "Ленина",
-            suite: "1-12"
-        },
-        phone: "89100997851",
-        website: "none"
-    }
     return (
         <>
             <UpperNavBar />
@@ -24,7 +13,11 @@ const About = () => {
                     <div className="col-md-4 mb-3">
                         <UserCard user={currentUser} avatar={true} />
                     </div>
-                    <div className="col-md-8"></div>
+                    <div className="col-md-8">
+                        {Object.values(additionalInfo).map((elem) => (
+                            <InfoCard info={elem} />
+                        ))}
+                    </div>
                 </div>
             </div>
         </>
---
+++ src/components/about/myInfo.js
@@ -0,0 +1,30 @@
+export const currentUser = {
+    username: "Igor_Aparin",
+    name: "Игорь Апарин",
+    email: "igor.aparin@bk.ru",
+    address: {
+        zipcode: 602265,
+        city: "Муром",
+        street: "Ленина",
+        suite: "1-12"
+    },
+    phone: "89100997851",
+    website: "https://github.com/nikrassel"
+    
+}
+
+export const additionalInfo = {
+    languages: {
+        title: "Языки программирования",
+        body: "Java Script, HTML, CSS, Python"
+    },
+    technologies: {
+        title: "Технологии",
+        body: "Работаю с технологиями React, Node.js, Redux, Bootstrap, Firebase, Docker, MongoDB, Postman."
+    },
+    project: {
+        title: "Мои проекты",
+        body: "Разрабатываю проект шаблона сайта для интернет магазина. На сайте реализована система регистрации и авторизации пользователя, возможность собрать и оформить заказ, просмотр уже сделанных заказов, выбор избранных товаров. Ссылка на проект:",
+        link: "https://shop-app-d9977.web.app/"
+    }
+}
